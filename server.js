5e43546473abecffc1b791bd521da8edd8123682
David Aydin <david.aydin6@gmail.com>
2022-05-21 03:24:34 -0400

    Edit page now takes account for all the fields added onto the show page.

--- server.js
+++ server.js
@@ -41,7 +41,27 @@ app.get("/pokemon/:id", (req, res) => {
 app.put("/pokemon/:id", (req, res) => {
   pokemon[req.params.id].id = req.body.id;
   pokemon[req.params.id].name = req.body.name;
-  pokemon[req.params.id].type[0] = req.body.type1;
+
+  if (pokemon[req.params.id].type.length === 2) {
+    pokemon[req.params.id].type[0] = req.body.type1;
+    pokemon[req.params.id].type[1] = req.body.type2;
+  } else {
+    pokemon[req.params.id].type[0] = req.body.type1;
+  }
+
+  pokemon[req.params.id].stats.hp = req.body.hp;
+  pokemon[req.params.id].stats.attack = req.body.attack;
+  pokemon[req.params.id].stats.defense = req.body.defense;
+  pokemon[req.params.id].stats.spattack = req.body.spattack;
+  pokemon[req.params.id].stats.spdefense = req.body.spdefense;
+  pokemon[req.params.id].stats.speed = req.body.speed;
+
+  pokemon[req.params.id].misc.classification = req.body.classification;
+  pokemon[req.params.id].misc.height = req.body.height;
+  pokemon[req.params.id].misc.weight = req.body.weight;
+  pokemon[req.params.id].misc.capturerate = req.body.capturerate;
+  pokemon[req.params.id].misc.eggsteps = req.body.eggsteps;
+
   res.redirect("/pokemon");
 });
 // Edit
